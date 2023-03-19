a793e6a0b761b60e2b43f2d3d8ba92cb0e10924a
«Daria <mavrina.d@mail.ru>
2023-03-19 23:14:29 +0300

    задание 1

---
+++ task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿/* // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+int Numbers(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void GetNumber(int num)
+{
+    string listNumber = " ";
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        int numberUser = Numbers("Введите число: ");
+        listNumber = listNumber + numberUser + ", ";  
+
+        if (numberUser > 0)
+        {
+           count ++;
+        }
+    }
+    Console.WriteLine($"{listNumber} количество чисел больше 0 = {count}");
+}
+
+
+int NumM = Numbers("Введите общее количество чисел: ");
+
+GetNumber(NumM);
+
